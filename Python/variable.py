b97583c5f71da07816b50087cc0b99733fbe00a0
Irteja Mahmud <irtejamahamud9@gmail.com>
2024-06-01 23:43:50 +0600

    first commit

---
+++ Python/variable.py
@@ -0,0 +1,27 @@
+x=5
+y="jhon"
+z=str(34)
+print(x)
+print(y)
+print(z)
+print(type(x))
+print(type(y))
+#legal expression
+myvar="irteja"
+_mayvar="irteja"
+myvar_2='irteja'
+
+a,b,c = 1,"irteja",6
+
+print(a, b, c)
+print(f"{a}, {b}, {c}")
+
+fruit=["apple","banana","mango"]
+a,b,c=fruit
+print(a+b+c)
+x="awsome"
+def myfun():
+    print("inside function "+x)
+
+myfun()
+print("Python is " + x)
